77d18aed7a7978d6b018f7d96df4d317e9c65273
vxndtt <alenakad16@mail.ru>
2024-09-20 15:12:50 +0500

    Update module_6_1.py
    Домашнее задание по теме "Зачем нужно наследование"
    Если вы решали старую версию задачи, проверка б

--- module_6_1.py
+++ module_6_1.py
@@ -5,6 +5,14 @@ class Animal:
     def __init__(self, name):
         self.name = name
 
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
 
 class Plant:
     edible = False
@@ -13,28 +21,13 @@ class Plant:
         self.name = name
 
 class Mammal(Animal):
-
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+    pass
 
 class Predator(Animal):
-
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
-
+    pass
 
 class Flower(Plant):
-    edible = True
+    pass
 
 class Fruit(Plant):
     edible = True
